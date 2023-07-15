f1930ab4c0ce3d0acb034db9c39a26e104edbf6a
Aakarshit <aakarshit.agarwal@gmail.com>
2023-07-15 20:37:44 +0530

    Adding FundMe contract

---
+++ contracts/FundMe.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract FundMe {
+    uint256 private constant MINIMUM_USD = 5e18;
+
+    function fund() public payable {
+        require(msg.value >= MINIMUM_USD, "You need to spend more ETH!");
+    }
+
+    function getVersion() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+        return priceFeed.version();
+    }
+
+    function getPrice() private view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        return uint256(answer * 1e10);
+    }
+
+    function getConversionRate(
+        uint256 ethAmount
+    ) private view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+}
